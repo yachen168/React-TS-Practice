6b241d0e0b4b05d749bc38fa11399d53a1c88006
yachen <io260188@gmail.com>
2021-04-03 03:06:36 +0800

    feat: initial Robot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import robots from "./mock/robots.json";
+
+import Robot from "./components/Robot";
+
+import "./App.css";
 
 function App(a) {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <ul>
+        {robots.map((robot) => (
+          <Robot id={robot.id} email={robot.email} name={robot.name} />
+        ))}
+      </ul>
     </div>
   );
 }
